b764dfefb8a296379fc0242da8a2023ac5ecbe07
Chris Wei <yunzhi.wei@live.com>
2016-11-24 18:31:57 +0800

    Start to use directive

---
+++ app/scripts/directives.js
@@ -0,0 +1,14 @@
+
+'use strict';
+
+angular.module('week3App')
+
+.directive('paymentPlanList', function() {
+  var ddo = {
+    templateUrl: 'directives/paymentplanlist.html'
+  };
+
+  return ddo;
+})
+
+;
