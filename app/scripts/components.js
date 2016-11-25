f0942d7474d3266572e36004650848f71fa39279
Chris Wei <yunzhi.wei@live.com>
2016-11-25 15:04:39 +0800

    more bindings and more lifecycle functions

--- app/scripts/components.js
+++ app/scripts/components.js
@@ -8,6 +8,8 @@ angular.module('week3App')
     controller: PaymentPlanListController,
     bindings: {
       plans: '<',
+      planlistlength: '<',
+      ischeckboxenable: '<',
       title: '@'
     }
 })
@@ -17,7 +19,31 @@ PaymentPlanListController.$inject = [];
 function PaymentPlanListController() {
   var $ctrl = this;
 
-  $ctrl.totalamountofselecteditems = 0;
+  $ctrl.$onInit = function () {
+    $ctrl.totalamountofselecteditems = 0;
+    $ctrl.totalamount = 0;
+  };
+
+  $ctrl.$onChanges = function (changeObj) {
+    console.log("changeObj: ", changeObj);
+    console.log("changeObj.plans: ", changeObj.plans);
+    console.log("changeObj.planlistlength: ", changeObj.planlistlength);
+    console.log("$ctrl.plans: ", $ctrl.plans, $ctrl.plans[0], $ctrl.plans[1]);
+    console.log("totalamount: ", $ctrl.totalamount);
+
+    if (changeObj.planlistlength.currentValue > 0) {
+      console.log("map reduce");
+      $ctrl.totalamount = $ctrl.plans
+      .map(function(item) {
+        console.log("item.amount: ", item.amount);
+        return item.amount;
+      })
+      .reduce(function(accu, value) {
+        console.log(accu, " + ", value, " = ", accu + value);
+        return accu + value;
+      }, 0);
+    }
+  }
 
   $ctrl.updateSelection = function($event, id) {
     var checkbox = $event.target;
@@ -31,5 +57,4 @@ function PaymentPlanListController() {
     }
     console.log("total amount: ", $ctrl.totalamountofselecteditems);
   };
-
 }
