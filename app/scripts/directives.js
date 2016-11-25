29fb31e6b9f741959196fb270adf0409813f3ebe
Chris Wei <yunzhi.wei@live.com>
2016-11-25 08:42:15 +0800

    Use scope to make the directive more flexible

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -5,7 +5,11 @@ angular.module('week3App')
 
 .directive('paymentPlanList', function() {
   var ddo = {
-    templateUrl: 'directives/paymentplanlist.html'
+    templateUrl: 'directives/paymentplanlist.html',
+    scope: {
+      bills: '=theBills',
+      title: '@title'
+    }
   };
 
   return ddo;
