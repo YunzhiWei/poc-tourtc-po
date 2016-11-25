67c0a4c748d7838bd3e77aeb1d06a7b4d7ddfb7a
Chris Wei <yunzhi.wei@live.com>
2016-11-25 17:22:07 +0800

    Add function of 'show payment plan list'

--- app/components/paymentbilllist.js
+++ app/components/paymentbilllist.js
@@ -13,7 +13,13 @@ angular.module('week3App')
 })
 ;
 
-PaymentBillListController.$inject = [];
-function PaymentBillListController() {
+PaymentBillListController.$inject = ['paymentBillService'];
+function PaymentBillListController(paymentBillService) {
   var $ctrl = this;
+
+  $ctrl.showDetails = function (id) {
+    $ctrl.detailBillId = id;
+    $ctrl.paymentPlans = paymentBillService.restPaymentPlans().query({bill_number:parseInt(id,10)});
+  }
+
 }
