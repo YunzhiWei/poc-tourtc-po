8750ccb71d2e23b662fb7efe637e3889365003c5
Chris Wei <yunzhi.wei@live.com>
2016-11-25 09:55:11 +0800

    Add accumulate total amount function in the directive

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -22,4 +22,20 @@ angular.module('week3App')
 
 function PaymentPlanListController() {
   var pplist = this;
+
+  pplist.totalamountofselecteditems = 0;
+
+  pplist.updateSelection = function($event, id) {
+    var checkbox = $event.target;
+    if (checkbox.checked) {
+      console.log("checked: ", id);
+      pplist.totalamountofselecteditems += pplist.plans[id].amount;
+    }
+    else {
+      console.log("unchecked: ", id);
+      pplist.totalamountofselecteditems -= pplist.plans[id].amount;
+    }
+    console.log("total amount: ", pplist.totalamountofselecteditems);
+  };
+
 }
