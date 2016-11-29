5e7400abe15adbe851d449953764c1a187690ace
Chris Wei <yunzhi.wei@live.com>
2016-11-29 18:37:33 +0800

    To track if the authority setting is changed

--- app/components/authoritylist.js
+++ app/components/authoritylist.js
@@ -19,23 +19,42 @@ AuthorityListController.$inject = [];
 function AuthorityListController() {
   var $ctrl = this;
 
+  $ctrl.$onInit = function () {
+    $ctrl.originalcode = $ctrl.rolecode.slice();
+  }
+
+  function isCodeChanged() {
+    var len = $ctrl.originalcode.length;
+
+    console.log("isCodeChanged(): ", $ctrl.originalcode, $ctrl.rolecode);
+
+    for (var i = 0; i < len; i++) {
+      if ($ctrl.originalcode[i] != $ctrl.rolecode[i]) return true;
+    }
+    return false;
+  }
+
   $ctrl.isSelected = function (groupid, code) {
-    console.log("rolecode: ", $ctrl.rolecode, "code: ", code);
+    // console.log("rolecode: ", $ctrl.rolecode, "code: ", code);
     return $ctrl.rolecode[groupid] & code;
   }
 
   $ctrl.updateSelection = function($event, groupid, code) {
     var checkbox = $event.target;
     if (checkbox.checked) {
-      console.log("checked: ", groupid, code);
+      // console.log("checked: ", groupid, code);
       $ctrl.rolecode[groupid] |= code;
     }
     else {
-      console.log("unchecked: ", groupid, code);
+      // console.log("unchecked: ", groupid, code);
       $ctrl.rolecode[groupid] &= (~code);
     }
 
-    console.log($ctrl.rolecode);
+    // console.log($ctrl.rolecode);
+
+    $ctrl.isCodeChanged = isCodeChanged();
+
+    console.log("isCodeChanged: ", $ctrl.isCodeChanged);
   };
 
   $ctrl.onClickAddGroup = function() {
@@ -56,6 +75,10 @@ function AuthorityListController() {
 
   $ctrl.onClickCreateNewRole = function () {
     console.log("onClickCreateNewRole");
+  };
+
+  $ctrl.onClickSaveChange = function () {
+    console.log("onClickSaveChange");
   };
 
 }
